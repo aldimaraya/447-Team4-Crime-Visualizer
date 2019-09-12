752eafa2be8a5ac384ef6e709b7e7a1bb170d320
Noah Jaffe <Noah0m0jaffe@gmail.com>
2019-09-12 16:41:52 -0400

    Added some route decorators and with blueprints for examples

--- backend/application.py
+++ backend/application.py
@@ -1,22 +1,66 @@
-import builtins
+import logging
+from flask import url_for
 from flask import Flask
 
-# create the flask app in the builtins scope so that all api versions have
-# access to it
-builtins.application = Flask(__name__)
+from defaults import defaultBlueprint
 
-# To prevent pep8 from changing the imports add this to the settings config:
-#   "python.formatting.autopep8Args": ["--ignore", "E402"]
+# Init flask application
+application = Flask(__name__)
+# Register blueprints
+application.register_blueprint(defaultBlueprint)
 
-# IMPORT APIs
-from defaults import *
 
-application = builtins.application
+def has_no_empty_params(rule):
+    """
+    Helper function for `get_home()`. dont feel like explaining lol 
+    """
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+    
+@application.route("/", methods=['GET'])
+def get_home():
+    """
+    The default home page. Display a site map
+    """
+    map_html = "Site Map: <br/>"
+    links = []
+    for rule in application.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if (("GET" in rule.methods) or ("POST" in rule.methods)) and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append(url)
+    # links is now a list of url, endpoint tuples
+    for item in sorted(links):
+        map_html += "<a href=\"" + item + \
+            "\" rel = \"nofollow noreferrer\" >" + item + "</a><br/>"
+    return map_html
+
+@application.after_request
+def after_request(response):
+    """
+    Copied code from StackOverflow to solve the 'POST' 'OPTION' issue.
+    Adding these headers fixes CORS errors. 
+    """
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 
 # run the app.
 if __name__ == "__main__":
+    logging.basicConfig(
+        # file = 'backendLogs.log'  # log to file # TODO: UNCOMMENT LINE FOR PRODUCTION
+        level=logging.INFO,
+        format='%(asctime)s.%(msecs)03d,%(levelname)s,%(module)s,%(funcName)s,%(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+    logging.info('Started app')
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application = builtins.application
     application.debug = True
     application.run()
+    logging.info("closing app")
--- backend/defaults.py
+++ backend/defaults.py
@@ -1,36 +1,13 @@
 from flask import jsonify
 from flask import request
-from flask import url_for
-from builtins import application as app
+from flask import Blueprint
 
+defaultBlueprint = Blueprint('defaults', __name__)
+@defaultBlueprint.route('/<loc>')
+def default_loc(loc):
+    return jsonify(loc)
 
-@app.route("/", methods=['GET'])
-def get_home():
-    """
-    The default home page. Display a site map
-    """
-    map_html = "Site Map: <br/>"
-    links = []
-    for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
-        if (("GET" in rule.methods) or ("POST" in rule.methods)) and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append(url)
-    # links is now a list of url, endpoint tuples
-    for item in sorted(links):
-        map_html += "<a href=\"" + item + \
-            "\" rel = \"nofollow noreferrer\" >" + item + "</a><br/>"
-    return map_html
-
-
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
-
-
-@app.errorhandler(404)
+@defaultBlueprint.app_errorhandler(404)
 def uri_not_found(e):
     """
     :returns: A jsonified error message for a 404 not found error. 
@@ -38,7 +15,7 @@ def uri_not_found(e):
     return jsonify(error=str(e)), 404
 
 
-@app.errorhandler(400)
+@defaultBlueprint.app_errorhandler(400)
 def resource_not_found(e):
     """
     Error handler for error code 400. Our API's should abort with an error 400 
@@ -52,14 +29,4 @@ def resource_not_found(e):
     return jsonify(error=str(e) + " Check the spelling of the resource requested."), 400
 
 
-@app.after_request
-def after_request(response):
-    """
-    Copied code from StackOverflow to solve the 'POST' 'OPTION' issue.
-    Adding these headers fixes CORS errors. 
-    """
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+
