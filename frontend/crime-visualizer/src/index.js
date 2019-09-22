add1d74fa9ebbbce405dd6cb451d6b14ca709482
Aldi Maraya <aldimaraya@gmail.com>
2019-09-22 18:19:56 -0400

    File Cleanup

--- frontend/crime-visualizer/src/index.js
+++ frontend/crime-visualizer/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Map from './js/map.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('map'));
+ReactDOM.render(<Map />, document.getElementById('map'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
